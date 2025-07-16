321115553766d1fe87263d057bebcf63a0d7caf8
daniellamarr <danielchidiebele@gmail.com>
2025-07-16 10:08:27 +0100

    fix entry issues

--- src/app/manifesto/page.tsx
+++ src/app/manifesto/page.tsx
@@ -1,5 +1,3 @@
-import Subscribe from "@/components/subscribe";
-import AllSections from "../_components/all-sections";
 import Manifesto from "./manifesto";
 
 export default function ManifestoPage() {
